c9b06267b694d5873c35b11607c7132eea903193
Gabriel <g4briel.4lves@gmail.com>
2019-07-22 00:48:51 +0000

    Atualizando .gitignore e scripts de configuração

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 
+echo "Checking if migration directory exists..."
+if [ ! -d "/site-secomp/migrations" ]; then
+   flask db init
+fi
+
 echo "Checking for schema updates..."
-flask db migrate && flask db upgrade 
+flask db migrate && flask db upgrade
+
 echo "Starting up Gunicorn..."
 exec gunicorn -w ${GUNICORN_WORKERS} -b :8000 app:'create_app()'
