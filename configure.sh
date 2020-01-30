f9e8c913a3814993912e720edea0c775337b55e1
Pabolo Pires <pabolo18@gmail.com>
2020-01-30 18:04:25 -0300

    Limpando o arquivo .configure

--- configure.sh
+++ configure.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
+# Caminho para o arquivo template do "nginx.conf"
+NGINX_CONF_TEMPLATE_PATH="./volumes/nginx/nginx.conf.template"
+# Caminho para o arquivo "nginx.conf"
+NGINX_CONF_PATH="./volumes/nginx/nginx.conf"
+# Camingo para o arquivo ".env"
+DOT_ENV_PATH="./.env"
+
 echo "Configurando o Docker"
 
 echo "Configurando Nginx"
-echo -n "- Entre SERVER_NAMES (localhost): "
-read SERVER_NAMES
+read -p "- Entre SERVER_NAMES (localhost): " SERVER_NAMES
 if [ -z $SERVER_NAMES ]
 then
     SERVER_NAMES=localhost
 fi
 
-echo -n "- Entre SERVER_DOMAIN (localhost): "
-read SERVER_DOMAIN
+read -p "- Entre SERVER_DOMAIN (localhost): " SERVER_DOMAIN
 if [ -z $SERVER_DOMAIN ]
 then
     SERVER_DOMAIN=localhost
 fi
 
-cp ./volumes/nginx/nginx.conf.template ./volumes/nginx/nginx.conf
-sed --in-place "s/%(SERVER_NAMES)/$SERVER_NAMES/g" ./volumes/nginx/nginx.conf
-sed --in-place "s/%(SERVER_DOMAIN)/$SERVER_DOMAIN/g" ./volumes/nginx/nginx.conf
+# Criando arquivo de configurações
+cp $NGINX_CONF_TEMPLATE_PATH $NGINX_CONF_PATH
+# Substituindo váriaveis no template
+sed --in-place "s/%(SERVER_NAMES)/$SERVER_NAMES/g" $NGINX_CONF_PATH
+sed --in-place "s/%(SERVER_DOMAIN)/$SERVER_DOMAIN/g" $NGINX_CONF_PATH 
 
 
 echo "Configurando MySQL"
 
-echo -n "- Entre a senha do root (root): "
-read -s MYSQL_ROOT_PASSWORD 
+read -s -p "- Entre a senha do root (root): " MYSQL_ROOT_PASSWORD 
 echo
 if [ -z $MYSQL_ROOT_PASSWORD ]
 then
     MYSQL_ROOT_PASSWORD="root"
 fi
 
-echo -n "- Entre o nome do banco de dados (db): "
-read MYSQL_DATABASE 
+read -p "- Entre o nome do banco de dados (db): " MYSQL_DATABASE 
 if [ -z $MYSQL_DATABASE ]
 then
     MYSQL_DATABASE="db"
 fi
 
-echo -n "- Entre o nome de usuário (root): "
-read MYSQL_USER
+read -p "- Entre o nome de usuário (root): " MYSQL_USER
 if [ -z $MYSQL_USER ]
 then
     MYSQL_USER="root"
 fi
 
-echo -n "- Entre a senha (root): "
-read -s MYSQL_PASSWORD
+read -s -p "- Entre a senha (root): " MYSQL_PASSWORD
 echo
 if [ -z $MYSQL_PASSWORD ]
 then
@@ -55,15 +58,15 @@ then
 fi
 
 echo "Configurando ambiente do Flask"
-echo -n "- Entre o nome do ambiente do Flask [development, production, default] (development): "
-read FLASK_ENVIRONMENT
+read -p "- Entre o nome do ambiente do Flask [development, production, default] (default): " FLASK_ENVIRONMENT
 if [ -z $FLASK_ENVIRONMENT ]
 then
-    FLASK_ENVIRONMENT="development"
+    FLASK_ENVIRONMENT="default"
 fi
 
-echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" > .env
-echo "MYSQL_DATABASE=$MYSQL_DATABASE" >> .env
-echo "MYSQL_USER=$MYSQL_USER" >> .env
-echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >> .env
-echo "FLASK_ENVIRONMENT=$FLASK_ENVIRONMENT" >> .env
+# Criando arquivo com váriaveis
+echo "MYSQL_ROOT_PASSWORD=\"$MYSQL_ROOT_PASSWORD\"" > $DOT_ENV_PATH
+echo "MYSQL_DATABASE=\"$MYSQL_DATABASE\"" >> $DOT_ENV_PATH
+echo "MYSQL_USER=\"$MYSQL_USER\"" >> $DOT_ENV_PATH
+echo "MYSQL_PASSWORD=\"$MYSQL_PASSWORD\"" >> $DOT_ENV_PATH
+echo "FLASK_ENVIRONMENT=\"$FLASK_ENVIRONMENT\"" >> $DOT_ENV_PATH
