5e8aa0d37026bdf67091eb3826220e99e6753cb7
Pabolo Pires <pabolo18@gmail.com>
2020-01-31 16:45:25 -0300

    Troca bash por sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Caminho para o arquivo template do "nginx.conf"
 NGINX_CONF_TEMPLATE_PATH="./volumes/nginx/nginx.conf.template"
@@ -10,13 +10,15 @@ DOT_ENV_PATH="./.env"
 echo "Configurando o Docker"
 
 echo "Configurando Nginx"
-read -p "- Entre SERVER_NAMES (localhost): " SERVER_NAMES
+echo -n "- Entre SERVER_NAMES (localhost): "
+read SERVER_NAMES
 if [ -z $SERVER_NAMES ]
 then
     SERVER_NAMES=localhost
 fi
 
-read -p "- Entre SERVER_DOMAIN (localhost): " SERVER_DOMAIN
+echo -n "- Entre SERVER_DOMAIN (localhost): "
+read SERVER_DOMAIN
 if [ -z $SERVER_DOMAIN ]
 then
     SERVER_DOMAIN=localhost
@@ -26,31 +28,39 @@ fi
 cp $NGINX_CONF_TEMPLATE_PATH $NGINX_CONF_PATH
 # Substituindo váriaveis no template
 sed --in-place "s/%(SERVER_NAMES)/$SERVER_NAMES/g" $NGINX_CONF_PATH
-sed --in-place "s/%(SERVER_DOMAIN)/$SERVER_DOMAIN/g" $NGINX_CONF_PATH 
+sed --in-place "s/%(SERVER_DOMAIN)/$SERVER_DOMAIN/g" $NGINX_CONF_PATH
 
 
 echo "Configurando MySQL"
 
-read -s -p "- Entre a senha do root (root): " MYSQL_ROOT_PASSWORD 
+echo -n "- Entre a senha do root (root): "
+stty -echo
+read MYSQL_ROOT_PASSWORD 
+stty echo
 echo
 if [ -z $MYSQL_ROOT_PASSWORD ]
 then
     MYSQL_ROOT_PASSWORD="root"
 fi
 
-read -p "- Entre o nome do banco de dados (db): " MYSQL_DATABASE 
+echo -n "- Entre o nome do banco de dados (db): "
+read MYSQL_DATABASE
 if [ -z $MYSQL_DATABASE ]
 then
     MYSQL_DATABASE="db"
 fi
 
-read -p "- Entre o nome de usuário (root): " MYSQL_USER
+echo -n "- Entre o nome de usuário (root): "
+read MYSQL_USER
 if [ -z $MYSQL_USER ]
 then
     MYSQL_USER="root"
 fi
 
-read -s -p "- Entre a senha (root): " MYSQL_PASSWORD
+echo -n "- Entre a senha (root): "
+stty -echo
+read MYSQL_PASSWORD
+stty echo
 echo
 if [ -z $MYSQL_PASSWORD ]
 then
@@ -58,7 +68,8 @@ then
 fi
 
 echo "Configurando ambiente do Flask"
-read -p "- Entre o nome do ambiente do Flask [development, production, default] (default): " FLASK_ENVIRONMENT
+echo -n "- Entre o nome do ambiente do Flask [development, production, default] (default): "
+read FLASK_ENVIRONMENT
 if [ -z $FLASK_ENVIRONMENT ]
 then
     FLASK_ENVIRONMENT="default"
