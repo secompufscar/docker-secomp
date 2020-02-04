c679b1cf8452f63850ceae575d6822fec40b4581
RosevalJr <54608835+RosevalJr@users.noreply.github.com>
2020-02-04 17:48:26 -0300

    Adicionado condições no script de configuração
    Em cada condição é checado se a variável já esta setada no ambiente, antes de fazer o read.

--- configure.sh
+++ configure.sh
@@ -10,18 +10,26 @@ DOT_ENV_PATH="./.env"
 echo "Configurando o Docker"
 
 echo "Configurando Nginx"
+
+# Caso variavel ja esteja setada, nao realizar o read.
+if [ -z $SERVER_NAMES ]
+then
 echo -n "- Entre SERVER_NAMES (localhost): "
 read SERVER_NAMES
 if [ -z $SERVER_NAMES ]
 then
     SERVER_NAMES=localhost
 fi
+fi
 
+if [ -z $SERVER_DOMAIN ]
+then
 echo -n "- Entre SERVER_DOMAIN (localhost): "
 read SERVER_DOMAIN
 if [ -z $SERVER_DOMAIN ]
 then
     SERVER_DOMAIN=localhost
+fi
 fi
 
 # Criando arquivo de configurações
@@ -33,6 +41,8 @@ sed --in-place "s/%(SERVER_DOMAIN)/$SERVER_DOMAIN/g" $NGINX_CONF_PATH
 
 echo "Configurando MySQL"
 
+if [ -z $MYSQL_ROOT_PASSWORD ]
+then
 echo -n "- Entre a senha do root (root): "
 stty -echo
 read MYSQL_ROOT_PASSWORD 
@@ -42,21 +52,30 @@ if [ -z $MYSQL_ROOT_PASSWORD ]
 then
     MYSQL_ROOT_PASSWORD="root"
 fi
+fi
 
+if [ -z $MYSQL_DATABASE ]
+then
 echo -n "- Entre o nome do banco de dados (db): "
 read MYSQL_DATABASE
 if [ -z $MYSQL_DATABASE ]
 then
     MYSQL_DATABASE="db"
 fi
+fi
 
+if [ -z $MYSQL_USER ]
+then
 echo -n "- Entre o nome de usuário (root): "
 read MYSQL_USER
 if [ -z $MYSQL_USER ]
 then
     MYSQL_USER="root"
 fi
+fi
 
+if [ -z $MYSQL_PASSWORD ]
+then
 echo -n "- Entre a senha do usuário (root): "
 stty -echo
 read MYSQL_PASSWORD
@@ -66,13 +85,17 @@ if [ -z $MYSQL_PASSWORD ]
 then
     MYSQL_PASSWORD="root"
 fi
+fi
 
+if [ -z $FLASK_ENVIRONMENT ]
+then
 echo "Configurando ambiente do Flask"
 echo -n "- Entre o nome do ambiente do Flask [development, production, default] (default): "
 read FLASK_ENVIRONMENT
 if [ -z $FLASK_ENVIRONMENT ]
 then
     FLASK_ENVIRONMENT="default"
+fi
 fi
 
 # Criando arquivo com váriaveis
