980174b0dd8cf59ea8d4a11903e476dbe450aa6e
RosevalJr <54608835+RosevalJr@users.noreply.github.com>
2020-02-10 02:39:04 -0300

    Adicionado indentação em cada if

--- configure.sh
+++ configure.sh
@@ -14,21 +14,25 @@ echo "Configurando Nginx"
 # Caso variavel ja esteja setada, nao realizar o read.
 if [ -z $SERVER_NAMES ]
 then
-echo -n "- Entre SERVER_NAMES (localhost): "
-read SERVER_NAMES
+    echo -n "- Entre SERVER_NAMES (localhost): "
+    read SERVER_NAMES
+    
 if [ -z $SERVER_NAMES ]
 then
     SERVER_NAMES=localhost
+    
 fi
 fi
 
 if [ -z $SERVER_DOMAIN ]
 then
-echo -n "- Entre SERVER_DOMAIN (localhost): "
-read SERVER_DOMAIN
+    echo -n "- Entre SERVER_DOMAIN (localhost): "
+    read SERVER_DOMAIN
+    
 if [ -z $SERVER_DOMAIN ]
 then
     SERVER_DOMAIN=localhost
+    
 fi
 fi
 
@@ -43,58 +47,68 @@ echo "Configurando MySQL"
 
 if [ -z $MYSQL_ROOT_PASSWORD ]
 then
-echo -n "- Entre a senha do root (root): "
-stty -echo
-read MYSQL_ROOT_PASSWORD 
-stty echo
-echo
+    echo -n "- Entre a senha do root (root): "
+    stty -echo
+    read MYSQL_ROOT_PASSWORD 
+    stty echo
+    echo
+    
 if [ -z $MYSQL_ROOT_PASSWORD ]
 then
     MYSQL_ROOT_PASSWORD="root"
+    
 fi
 fi
 
 if [ -z $MYSQL_DATABASE ]
 then
-echo -n "- Entre o nome do banco de dados (db): "
-read MYSQL_DATABASE
+    echo -n "- Entre o nome do banco de dados (db): "
+    read MYSQL_DATABASE
+    
 if [ -z $MYSQL_DATABASE ]
 then
     MYSQL_DATABASE="db"
+    
 fi
 fi
 
 if [ -z $MYSQL_USER ]
 then
-echo -n "- Entre o nome de usuário (root): "
-read MYSQL_USER
+    echo -n "- Entre o nome de usuário (root): "
+    read MYSQL_USER
+    
 if [ -z $MYSQL_USER ]
 then
     MYSQL_USER="root"
+    
 fi
 fi
 
 if [ -z $MYSQL_PASSWORD ]
 then
-echo -n "- Entre a senha do usuário (root): "
-stty -echo
-read MYSQL_PASSWORD
-stty echo
-echo
+    echo -n "- Entre a senha do usuário (root): "
+    stty -echo
+    read MYSQL_PASSWORD
+    stty echo
+    echo
+    
 if [ -z $MYSQL_PASSWORD ]
 then
     MYSQL_PASSWORD="root"
+    
 fi
 fi
 
 if [ -z $FLASK_ENVIRONMENT ]
 then
-echo "Configurando ambiente do Flask"
-echo -n "- Entre o nome do ambiente do Flask [development, production, default] (default): "
-read FLASK_ENVIRONMENT
+    echo "Configurando ambiente do Flask"
+    echo -n "- Entre o nome do ambiente do Flask [development, production, default] (default): "
+    read FLASK_ENVIRONMENT
+    
 if [ -z $FLASK_ENVIRONMENT ]
 then
     FLASK_ENVIRONMENT="default"
+
 fi
 fi
 
