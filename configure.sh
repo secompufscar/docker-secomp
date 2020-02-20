f9fa31064fa17d78badd6ac13a5aa5db14fd14a6
RosevalJr <54608835+RosevalJr@users.noreply.github.com>
2020-02-19 21:57:56 -0300

    Arrumado indentação e adicionado opção "--default"
    Utilizando a opção "--default" o script ja adota todos os valores default das variaveis utilizadas.

--- configure.sh
+++ configure.sh
@@ -7,33 +7,51 @@ NGINX_CONF_PATH="./volumes/nginx/nginx.conf"
 # Camingo para o arquivo ".env"
 DOT_ENV_PATH="./.env"
 
+# Checando se foi passado --default, para utilizar os valores default
+DEFAULT=0
+if [ "$1" = "--default" ]
+then
+    DEFAULT=1
+fi
+
 echo "Configurando o Docker"
 
 echo "Configurando Nginx"
 
-# Caso variavel ja esteja setada, nao realizar o read.
-if [ -z $SERVER_NAMES ]
+# Caso nao foi passado --default
+if [ $DEFAULT = 0 ]
 then
-    echo -n "- Entre SERVER_NAMES (localhost): "
-    read SERVER_NAMES
+    # Caso variavel ja esteja setada, nao realizar o read.
+    if [ -z $SERVER_NAMES ]
+    then
+        echo -n "- Entre SERVER_NAMES (localhost): "
+        read SERVER_NAMES
     
-if [ -z $SERVER_NAMES ]
-then
+        if [ -z $SERVER_NAMES ]
+        then
+            SERVER_NAMES=localhost
+    
+        fi
+    fi
+else
     SERVER_NAMES=localhost
-    
-fi
 fi
 
-if [ -z $SERVER_DOMAIN ]
+if [ $DEFAULT = 0 ]
 then
-    echo -n "- Entre SERVER_DOMAIN (localhost): "
-    read SERVER_DOMAIN
+    if [ -z $SERVER_DOMAIN ]
+    then
+        echo -n "- Entre SERVER_DOMAIN (localhost): "
+        read SERVER_DOMAIN
     
-if [ -z $SERVER_DOMAIN ]
-then
+        if [ -z $SERVER_DOMAIN ]
+        then
+            SERVER_DOMAIN=localhost
+    
+        fi
+    fi
+else
     SERVER_DOMAIN=localhost
-    
-fi
 fi
 
 # Criando arquivo de configurações
@@ -45,71 +63,96 @@ sed --in-place "s/%(SERVER_DOMAIN)/$SERVER_DOMAIN/g" $NGINX_CONF_PATH
 
 echo "Configurando MySQL"
 
-if [ -z $MYSQL_ROOT_PASSWORD ]
+if [ $DEFAULT = 0 ]
 then
-    echo -n "- Entre a senha do root (root): "
-    stty -echo
-    read MYSQL_ROOT_PASSWORD 
-    stty echo
-    echo
+    if [ -z $MYSQL_ROOT_PASSWORD ]
+    then
+        echo -n "- Entre a senha do root (root): "
+        stty -echo
+        read MYSQL_ROOT_PASSWORD 
+        stty echo
+        echo
     
-if [ -z $MYSQL_ROOT_PASSWORD ]
-then
+        if [ -z $MYSQL_ROOT_PASSWORD ]
+        then
+            MYSQL_ROOT_PASSWORD="root"
+    
+        fi
+    fi
+else
     MYSQL_ROOT_PASSWORD="root"
-    
-fi
 fi
 
-if [ -z $MYSQL_DATABASE ]
+if [ $DEFAULT = 0 ]
 then
-    echo -n "- Entre o nome do banco de dados (db): "
-    read MYSQL_DATABASE
+    if [ -z $MYSQL_DATABASE ]
+    then
+        echo -n "- Entre o nome do banco de dados (db): "
+        read MYSQL_DATABASE
     
-if [ -z $MYSQL_DATABASE ]
-then
+        if [ -z $MYSQL_DATABASE ]
+        then
+            MYSQL_DATABASE="db"
+    
+        fi
+    fi
+else
     MYSQL_DATABASE="db"
-    
-fi
 fi
 
-if [ -z $MYSQL_USER ]
+if [ $DEFAULT = 0 ]
 then
-    echo -n "- Entre o nome de usuário (root): "
-    read MYSQL_USER
+    if [ -z $MYSQL_USER ]
+    then
+        echo -n "- Entre o nome de usuário (root): "
+        read MYSQL_USER
     
-if [ -z $MYSQL_USER ]
-then
+        if [ -z $MYSQL_USER ]
+        then
+            MYSQL_USER="root"
+    
+        fi
+    fi
+else
     MYSQL_USER="root"
-    
-fi
 fi
 
-if [ -z $MYSQL_PASSWORD ]
+if [ $DEFAULT = 0 ]
 then
-    echo -n "- Entre a senha do usuário (root): "
-    stty -echo
-    read MYSQL_PASSWORD
-    stty echo
-    echo
+    if [ -z $MYSQL_PASSWORD ]
+    then
+        echo -n "- Entre a senha do usuário (root): "
+        stty -echo
+        read MYSQL_PASSWORD
+        stty echo
+        echo
     
-if [ -z $MYSQL_PASSWORD ]
-then
+        if [ -z $MYSQL_PASSWORD ]
+        then
+            MYSQL_PASSWORD="root"
+    
+        fi
+    fi
+else
     MYSQL_PASSWORD="root"
-    
-fi
 fi
 
-if [ -z $FLASK_ENVIRONMENT ]
+if [ $DEFAULT = 0 ]
 then
-    echo "Configurando ambiente do Flask"
-    echo -n "- Entre o nome do ambiente do Flask [development, production, default] (default): "
-    read FLASK_ENVIRONMENT
+    if [ -z $FLASK_ENVIRONMENT ]
+    then
+        echo "Configurando ambiente do Flask"
+        echo -n "- Entre o nome do ambiente do Flask [development, production, default] (default): "
+        read FLASK_ENVIRONMENT
     
-if [ -z $FLASK_ENVIRONMENT ]
-then
+        if [ -z $FLASK_ENVIRONMENT ]
+        then
+            FLASK_ENVIRONMENT="default"
+
+        fi
+    fi
+else
     FLASK_ENVIRONMENT="default"
-
-fi
 fi
 
 # Criando arquivo com váriaveis
