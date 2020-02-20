bc6f0220762bf5a43aac4d1eaece7187c14430a1
Gabriel Alves <g4briel.4lves@gmail.com>
2020-02-20 16:17:13 -0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ if [ "$1" = "--pull" ]; then
    git submodule foreach git pull
 fi
 docker-compose stop flask nginx
-docker-compose rm flask nginx
+docker-compose rm -f flask nginx
 docker rmi dockersecomp_flask:latest python:3-alpine nginx:alpine
 sudo chown ubuntu:ubuntu -R volumes/db/data
 docker-compose up --build  -d
